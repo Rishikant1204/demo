f85eceb972ef706142f5bc548cefefd447975341
Abhijeet_Sen <abhijeetsen1729@gmail.com>
2023-11-30 16:06:13 +0530

    core and feature files added

---
+++ lib/src/features/auth/presentation/getX/auth_getx.dart
@@ -0,0 +1,7 @@
+
+  library auth_getx;
+  export 'auth_controller.dart';
+  export 'auth_service.dart';
+  export 'auth_state.dart';
+  export 'auth_binding.dart';
+  
---
+++ lib/src/features/home/data/implements/implements.dart
@@ -0,0 +1,15 @@
+
+    import '../sources/sources.dart';
+    class HomeRepositoryImp {
+
+        final HomeRemoteDataSource remoteDataSource;
+        HomeRepositoryImp({required this.remoteDataSource});
+      
+        // ... example ...
+        //
+        // Future<User> getUser(String userId) async {
+        //     return remoteDataSource.getUser(userId);
+        //   }
+        // ...
+    }
+    
